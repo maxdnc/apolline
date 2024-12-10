b207da6fa9b3af72d8588fb8bd01a7e594395af5
Maxime Danic <maxime.danic@gmail.com>
2024-12-11 00:29:37 +0200

    Enhance Tailwind CSS configuration with hamburger animations and keyframes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,48 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      animation: {
+        'hamburger-top-bar':
+          'hamburgerAnimationTopBar 0.6s cubic-bezier(0.90, 0, 0.10, 1) forwards',
+        'hamburger-bottom-bar':
+          'hamburgerAnimationBottomBar 0.6s cubic-bezier(0.90, 0, 0.10, 1) forwards',
+        'hamburger-top-bar-close':
+          'hamburgerAnimationTopBarClose 0.6s cubic-bezier(0.90, 0, 0.10, 1) forwards',
+        'hamburger-bottom-bar-close':
+          'hamburgerAnimationBottomBarClose 0.6s cubic-bezier(0.90, 0, 0.10, 1) forwards',
+      },
+      keyframes: {
+        hamburgerAnimationTopBar: {
+          '0%': { transform: 'translateY(-4px)   rotate(0)' },
+          '50%': { transform: 'translateY(2px)  rotate(0)' },
+          '100%': { transform: 'translateY(2px)  rotate(45deg)' },
+        },
+        hamburgerAnimationBottomBar: {
+          '0%': { transform: 'translateY(4px) rotate(0)' },
+          '50%': { transform: 'translateY(-2px)  rotate(0)' },
+          '100%': { transform: 'translateY(-2px)  rotate(-45deg)' },
+        },
+        hamburgerAnimationTopBarClose: {
+          '0%': { transform: 'translateY(2px) rotate(45deg)' },
+          '50%': { transform: 'translateY(2px) rotate(0)' },
+          '100%': { transform: 'translateY(-4px) rotate(0)' },
+        },
+        hamburgerAnimationBottomBarClose: {
+          '0%': { transform: 'translateY(-2px) rotate(-45deg)' },
+          '50%': { transform: 'translateY(-2px) rotate(0)' },
+          '100%': { transform: 'translateY(4px) rotate(0)' },
+        },
       },
     },
   },
