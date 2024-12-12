2b75001d6689b2db8b9b9025d8fd3c26d237fa3b
Maxime Danic <maxime.danic@gmail.com>
2024-12-12 12:34:16 +0200

    Add SectionIntro component for enhanced homepage presentation and parallax effect

---
+++ src/app/(acceuil)/_components/SectionIntro.tsx
@@ -0,0 +1,66 @@
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import Background from '../../../../public/images/home/introSection.jpg';
+import { useScroll, useTransform, motion } from 'motion/react';
+import { useRef } from 'react';
+import MainButtonLink from '@/components/reusable-ui/MainButtonLink';
+
+export default function SectionIntro() {
+  const container = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start start', 'end start'],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], ['0vh', '60vh']);
+  const textY = useTransform(scrollYProgress, [0, 1], ['0vh', '-30vh']);
+  const opacity = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+  const opacityText = useTransform(scrollYProgress, [0, 0.4, 0.8], [1, 1, 0]);
+
+  return (
+    <div ref={container} className="h-screen w-full overflow-hidden">
+      <motion.div style={{ y }} className="relative h-full">
+        <Image
+          src={Background}
+          fill
+          alt="image"
+          style={{ objectFit: 'cover' }}
+        />
+        {/* Overlay */}
+        <motion.div
+          style={{ opacity }}
+          className="absolute inset-0 bg-black z-10 "
+        />
+        <div className="absolute inset-0 backdrop-blur " />
+
+        {/* Text content with parallax */}
+        <motion.div
+          style={{ y: textY }}
+          className="absolute inset-0 z-20 flex flex-col items-center justify-center px-4"
+        >
+          <motion.div
+            style={{ opacity: opacityText }}
+            className="max-w-4xl text-center space-y-6"
+          >
+            <h1 className="text-4xl md:text-[6vw] font-light tracking-wider text-white leading-none text-pretty ">
+              Révelez votre beauté sans chirugie
+            </h1>
+            <h2 className="text-white/90 text-sm md:text-lg">
+              Cabinet de soins esthétiques à Boulogne-Sur-Mer
+            </h2>
+            <div className="flex flex-col mx-auto max-w-sm sm:max-w-none sm:flex-row gap-4 justify-center pt-8">
+              <MainButtonLink href="/" size="lg" variant="secondary">
+                Rendez-vous en ligne
+              </MainButtonLink>
+              <MainButtonLink href="/" size="lg" variant="primary">
+                Contactez-nous
+              </MainButtonLink>
+            </div>
+          </motion.div>
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,47 +5,51 @@ import MainButtonLink from '@/components/reusable-ui/MainButtonLink';
 import microPeeling from '../../public/images/home/micropelling.png';
 import faceCare from '../../public/images/home/faceCare.png';
 import AccordionGroup from '@/components/reusable-ui/AccordionGroup';
+import SectionIntro from './(acceuil)/_components/SectionIntro';
 
 export default function Acceuil() {
   return (
-    <div className="flex gap-4 py-16">
-      <div className="p-8 flex flex-col gap-8 w-full max-w-[1120px] mx-auto">
-        <div className="flex flex-col gap-2">
-          <span className="text-lg ">FAQ</span>
-          <h3 className="text-4xl font-semibold ">
-            Les questions fréquentes de nos clients
-          </h3>
-        </div>
-        <AccordionGroup />
+    <>
+      <SectionIntro />
+      <div className="flex flex-col gap-4 pb-16">
+        <div className="p-8 flex flex-col gap-8 w-full max-w-[1120px] mx-auto">
+          <div className="flex flex-col gap-2">
+            <span className="text-lg ">FAQ</span>
+            <h3 className="text-4xl font-semibold ">
+              Les questions fréquentes de nos clients
+            </h3>
+          </div>
+          <AccordionGroup />
 
-        <div className="flex flex-col sm:flex-row gap-4 mx-auto justify-center max-w-lg">
-          <MainButtonLink href="/contact" size="lg">
-            Prendre rendez-vous
-          </MainButtonLink>
-          <MainButtonLink href="/services" variant="outline" size="lg">
-            En savoir plus
-          </MainButtonLink>
-        </div>
+          <div className="flex flex-col sm:flex-row gap-4 mx-auto justify-center max-w-lg">
+            <MainButtonLink href="/contact" size="lg">
+              Prendre rendez-vous
+            </MainButtonLink>
+            <MainButtonLink href="/services" variant="outline" size="lg">
+              En savoir plus
+            </MainButtonLink>
+          </div>
 
-        <div className="flex flex-col md:flex-row  gap-16 ">
-          <CardImage
-            href="/services/example"
-            title="
+          <div className="flex flex-col md:flex-row  gap-16 ">
+            <CardImage
+              href="/services/example"
+              title="
 Microneedling"
-            description="En stimulant la cellule, le Microneedling permet d’activer le processus de cicatrisation naturelle et la réduction des rides et ridules."
-            imageUrl={microPeeling}
-            alt="Description of image"
-          />
-          <CardImage
-            href="/services/example"
-            title="
+              description="En stimulant la cellule, le Microneedling permet d’activer le processus de cicatrisation naturelle et la réduction des rides et ridules."
+              imageUrl={microPeeling}
+              alt="Description of image"
+            />
+            <CardImage
+              href="/services/example"
+              title="
 Radiofréquence visage et cou​"
-            description="Technique esthétique non invasive de rajeunissement de la peau qui donne des résultats très similaires à ceux du lifting chirurgical."
-            imageUrl={faceCare}
-            alt="Description of image"
-          />
+              description="Technique esthétique non invasive de rajeunissement de la peau qui donne des résultats très similaires à ceux du lifting chirurgical."
+              imageUrl={faceCare}
+              alt="Description of image"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
