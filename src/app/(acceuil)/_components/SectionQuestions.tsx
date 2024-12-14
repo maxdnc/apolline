e2016e89b11b58d420c072888b5f6e32eaf23a4f
Maxime Danic <maxime.danic@gmail.com>
2024-12-14 20:40:25 +0200

    Add SectionQuestions component for displaying frequently asked questions with navigation links

---
+++ src/app/(acceuil)/_components/SectionQuestions.tsx
@@ -0,0 +1,23 @@
+import AccordionGroup from '@/components/reusable-ui/AccordionGroup';
+import MainButtonLink from '@/components/reusable-ui/MainButtonLink';
+import { SectionWrapper } from '@/components/reusable-ui/SectionWrapper';
+import TitleSection from '@/components/reusable-ui/TitleSection';
+
+const SectionQuestions = () => {
+  return (
+    <SectionWrapper>
+      <TitleSection>Les questions fréquentes de nos clients</TitleSection>
+      <AccordionGroup />
+
+      <div className="flex flex-col sm:flex-row  gap-4  mt-8">
+        <MainButtonLink href="/contact" size="lg">
+          Posez-nous vos questions
+        </MainButtonLink>
+        <MainButtonLink href="/services" variant="outline" size="lg">
+          Contactez nous
+        </MainButtonLink>
+      </div>
+    </SectionWrapper>
+  );
+};
+export default SectionQuestions;
