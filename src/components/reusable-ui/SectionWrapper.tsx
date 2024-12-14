43baab2b8089d25eb77235d436d81b64c1e5aba6
Maxime Danic <maxime.danic@gmail.com>
2024-12-14 20:03:47 +0200

    Add SectionWrapper component for flexible section layout and styling

---
+++ src/components/reusable-ui/SectionWrapper.tsx
@@ -0,0 +1,17 @@
+interface SectionWrapperProps {
+  children: React.ReactNode;
+  background?: string;
+}
+
+export const SectionWrapper = ({
+  children,
+  background,
+}: SectionWrapperProps) => {
+  return (
+    <section
+      className={`py-[60px] px-[40px] lg:px-[70px] lg:py-[120px] ${background}`}
+    >
+      <div className="max-w-largest mx-auto">{children}</div>
+    </section>
+  );
+};
