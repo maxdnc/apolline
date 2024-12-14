75fcfddc41dcc5376d9966a1e7ec8bb579f41cb2
Maxime Danic <maxime.danic@gmail.com>
2024-12-14 20:04:15 +0200

    Add SectionPresentation and TitleSection components; restructure page layout with SectionWrapper for improved service display and FAQ section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,50 +6,59 @@ import microPeeling from '../../public/images/home/micropelling.png';
 import faceCare from '../../public/images/home/faceCare.png';
 import AccordionGroup from '@/components/reusable-ui/AccordionGroup';
 import SectionIntro from './(acceuil)/_components/SectionIntro';
+import SectionPresentation from './(acceuil)/_components/SectionPresentation';
+import TitleSection from '@/components/reusable-ui/TitleSection';
+import { SectionWrapper } from '@/components/reusable-ui/SectionWrapper';
 
 export default function Acceuil() {
   return (
     <>
       <SectionIntro />
-      <div className="flex flex-col gap-4 pb-16">
-        <div className="p-8 flex flex-col gap-8 w-full max-w-[1120px] mx-auto">
-          <div className="flex flex-col gap-2">
-            <span className="text-lg ">FAQ</span>
-            <h3 className="text-4xl font-semibold ">
-              Les questions fréquentes de nos clients
-            </h3>
-          </div>
-          <AccordionGroup />
+      <SectionPresentation />
 
-          <div className="flex flex-col sm:flex-row gap-4 mx-auto justify-center max-w-lg">
-            <MainButtonLink href="/contact" size="lg">
-              Prendre rendez-vous
-            </MainButtonLink>
-            <MainButtonLink href="/services" variant="outline" size="lg">
-              En savoir plus
-            </MainButtonLink>
-          </div>
-
-          <div className="flex flex-col md:flex-row  gap-16 ">
-            <CardImage
-              href="/services/example"
-              title="
+      <SectionWrapper background="bg-slate-100">
+        <TitleSection>Nos Services</TitleSection>
+        <div className="flex flex-col md:flex-row  gap-16 mb-12 ">
+          <CardImage
+            href="/services/example"
+            title="
 Microneedling"
-              description="En stimulant la cellule, le Microneedling permet d’activer le processus de cicatrisation naturelle et la réduction des rides et ridules."
-              imageUrl={microPeeling}
-              alt="Description of image"
-            />
-            <CardImage
-              href="/services/example"
-              title="
+            description="En stimulant la cellule, le Microneedling permet d’activer le processus de cicatrisation naturelle et la réduction des rides et ridules."
+            imageUrl={microPeeling}
+            alt="Description of image"
+          />
+          <CardImage
+            href="/services/example"
+            title="
 Radiofréquence visage et cou​"
-              description="Technique esthétique non invasive de rajeunissement de la peau qui donne des résultats très similaires à ceux du lifting chirurgical."
-              imageUrl={faceCare}
-              alt="Description of image"
-            />
-          </div>
+            description="Technique esthétique non invasive de rajeunissement de la peau qui donne des résultats très similaires à ceux du lifting chirurgical."
+            imageUrl={faceCare}
+            alt="Description of image"
+          />
+          <CardImage
+            href="/services/example"
+            title="
+Radiofréquence visage et cou​"
+            description="Technique esthétique non invasive de rajeunissement de la peau qui donne des résultats très similaires à ceux du lifting chirurgical."
+            imageUrl={faceCare}
+            alt="Description of image"
+          />
         </div>
-      </div>
+      </SectionWrapper>
+
+      <SectionWrapper>
+        <TitleSection>Les questions fréquentes de nos clients</TitleSection>
+        <AccordionGroup />
+
+        <div className="flex flex-col sm:flex-row  gap-4  mt-8">
+          <MainButtonLink href="/contact" size="lg">
+            Prendre rendez-vous
+          </MainButtonLink>
+          <MainButtonLink href="/services" variant="outline" size="lg">
+            En savoir plus
+          </MainButtonLink>
+        </div>
+      </SectionWrapper>
     </>
   );
 }
